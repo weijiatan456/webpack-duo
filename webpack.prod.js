f0568f858169ab9d9adb902080703d16c766e410
魏佳探 <442774948@qq.com>
2018-07-30 15:50:48 +0800

    参照vue-cli做一些版本依赖更新

--- webpack.prod.js
+++ webpack.prod.js
@@ -49,20 +49,13 @@ const plugins = [
         }
     }),
     new UglifyJSPlugin({
-        // 最紧凑的输出
-        beautify: false,
-        // 删除所有的注释
-        comments: false,
-        compress: {
-            // 在UglifyJs删除没有用到的代码时不输出警告
-            warnings: false,
-            // 删除所有的 `console` 语句 还可以兼容ie浏览器
-            drop_console: false,
-            // 内嵌定义了但是只用到一次的变量
-            collapse_vars: true,
-            // 提取出出现多次但是没有定义成变量去引用的静态值
-            reduce_vars: true
-        }
+        uglifyOptions: {
+            compress: {
+                warnings: false
+            }
+        },
+        sourceMap: false,
+        parallel: true
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.

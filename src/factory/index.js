7e19be7dda29c8f70eb1742f625c5e3461b06c1b
魏佳探 <442774948@qq.com>
2018-08-01 12:22:24 +0800

    增加了layer目录，单有jquery和layer两个依赖的写法

--- src/factory/index.js
+++ src/factory/index.js
@@ -1,4 +1,4 @@
-require("../utils/factory");
+require("./factory");
 
 $('.aaa').green();
 
---
+++ src/layer/index.js
@@ -0,0 +1,6 @@
+require("./layerFun");
+import '../utils/theme/default/layer.css';
+
+$('.aaa').green();
+
+console.log($.sum(99,88));
